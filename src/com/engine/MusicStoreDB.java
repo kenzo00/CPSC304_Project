2875e0737fb3243b01d956850d8bb94f9f515056
Kenneth Poon <kennethpoon7133@live.hk>
2014-11-06 20:59:08 -0800

    Added some stuff to test connections to local mySQL database

--- src/com/engine/MusicStoreDB.java
+++ src/com/engine/MusicStoreDB.java
@@ -1,9 +1,33 @@
 package com.engine;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class MusicStoreDB {
 
 	public static void main ( String[] args )
 	{
 		System.out.println("YAY FIRST CLASS :D");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		// URL is in format "jdbc:mysql://<ip>:<port>/<databasename>"
+    	String connectURL = "jdbc:mysql://localhost:3306/test"; 
+    	System.out.println("Please Enter your username");
+    	String username = scanner.nextLine();
+    	System.out.println("Please Enter your passowrd");
+    	String password = scanner.nextLine();
+    	
+		try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			Connection con = DriverManager.getConnection(connectURL,username,password);
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("Message: " + e.getMessage());
+			System.exit(-1);
+		}
+
 	}
 }
