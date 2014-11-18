d81dabd059ac7d4ab52d457fa636d296372cfc50
ivitaminb <ivitaminb@dhcp-206-87-223-82.ubcsecure.wireless.ubc.ca>
2014-11-18 15:59:57 -0800

    Return class

--- src/com/engine/Return.java
+++ src/com/engine/Return.java
@@ -1,11 +1,35 @@
 package com.engine;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
 public class Return {
-	
-	
+	private Connection connection;
+	// Constructor
 	public Return() {
 		
 	}
 	
+	public boolean withinDate() {
+		String query = "SELECT * FROM cpsc304.Order WHERE DateB >= DATEADD(DAY, -15, DateA)";
+		boolean bool = false;
+		ResultSet result;
+		try {
+			// prepare and execute the SELECT
+			PreparedStatement ps = connection.prepareStatement( query );
+			result = ps.executeQuery();
+			
+			while (result.next()) {
+				bool = true;
+			} 
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+		return bool;	
+	}
 	
 }
