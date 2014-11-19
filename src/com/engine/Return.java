98be1aceb7414bd538f34d1e6abc749ac0a1975e
Kenneth Poon <kennethpoon7133@live.hk>
2014-11-18 16:25:57 -0800

    Update Return class

--- src/com/engine/Return.java
+++ src/com/engine/Return.java
@@ -1,35 +1,56 @@
 package com.engine;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.mysql.jdbc.Connection;
 
-public class Return {
+public class Return 
+{
+	
 	private Connection connection;
+	
 	// Constructor
-	public Return() {
+	public Return() 
+	{
 		
 	}
 	
-	public boolean withinDate() {
-		String query = "SELECT * FROM cpsc304.Order WHERE DateB >= DATEADD(DAY, -15, DateA)";
-		boolean bool = false;
-		ResultSet result;
-		try {
+	// Checks if the purchase was made within 15 days
+	public boolean withinDate( int receiptId, Date currentDate ) 
+	{
+		String query = "SELECT date FROM Order WHERE receiptId=" + receiptId;
+		boolean isWithinDate = false;
+		
+		try 
+		{
 			// prepare and execute the SELECT
 			PreparedStatement ps = connection.prepareStatement( query );
-			result = ps.executeQuery();
+			ResultSet result = ps.executeQuery();
 			
-			while (result.next()) {
-				bool = true;
-			} 
+			Date date;
+			
+			result.next();
+			date = result.getDate( 2 );
+			
+			isWithinDate = day15DaysAfter ( date, currentDate );
 		}
-		catch (SQLException e){
-			e.printStackTrace();
+		catch (SQLException e)
+		{
+			System.out.println( "Failed to execute select statement:\n" + query );
 		}
-		return bool;	
+		return isWithinDate;
+	}
+	
+	// Returns true if firstDate is 15 days after secondDate
+	// Returns false otherwise
+	public boolean day15DaysAfter( Date firstDate, Date secondDate )
+	{
+		boolean is15DaysAfter = false;
+		
+		return is15DaysAfter;
 	}
 	
 }
