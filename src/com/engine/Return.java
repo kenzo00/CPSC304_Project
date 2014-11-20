085ab3f70ba7d2a6a74cf2c67b02a1a1b3525296
ivitaminb <ivitaminb@dhcp-206-87-217-101.ubcsecure.wireless.ubc.ca>
2014-11-19 18:25:44 -0800

    rough return

--- src/com/engine/Return.java
+++ src/com/engine/Return.java
@@ -12,15 +12,22 @@ public class Return
 {
 	
 	private Connection connection;
+	int success = 1;
 	
 	// Constructor
 	public Return() 
 	{
 		
 	}
-	
+	public int refund(int receiptId, int upc) {
+		
+		if (isValid(receitptId, upc) == success) {
+			// increment stock, and update table that return is made 
+			String query = "UPDATE stored WHERE upc=" + upc + ",receiptId=" + receiptId //and increment stock; 
+		}
+	}
 	// Checks if the purchase was made within 15 days
-	public boolean withinDate( int receiptId ) 
+	public int isValid( int receiptId, int upc ) //also check upc
 	{
 		String query = "SELECT date FROM Order WHERE receiptId=" + receiptId;
 		boolean isWithinDate = false;
@@ -36,11 +43,18 @@ public class Return
 			// Get the current date
 			LocalDate currentDate = LocalDate.now();
 			
-			result.next();
-			date = result.getDate( 2 );
-			
-			isWithinDate = !day15DaysAfter ( date.toLocalDate(), currentDate );
-		}
+			boolean hasNext;
+			hasNext = result.next();
+			// checks if there are any matching receiptID's
+			if (hasNext == true) {
+				date = result.getDate( 2 );
+				
+				isWithinDate = !day15DaysAfter ( date.toLocalDate(), currentDate );
+
+			}
+			else 
+				return 1;
+					}
 		catch (SQLException e)
 		{
 			System.out.println( "Failed to execute select statement:\n" + query );
@@ -63,5 +77,9 @@ public class Return
 		
 		return is15DaysAfter;
 	}
+	
+	// prompt message to say refunded to credit card
+	// check if item has already been returned
+	
 	
 }
