f76d3306bad485802f5e38a0eccd9edbcd9b2c97
Kenneth Poon <kennethpoon7133@live.hk>
2014-11-20 16:44:25 -0800

    Implement basic GUI interface

---
+++ src/com/gui/CustomerPanel.java
@@ -0,0 +1,15 @@
+package com.gui;
+
+import java.awt.ScrollPane;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class CustomerPanel extends JPanel
+{
+
+	public CustomerPanel()
+	{
+		
+	}
+}
