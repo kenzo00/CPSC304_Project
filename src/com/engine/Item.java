b90cb9a9661075fda00c6ec8e58537f2a1958861
Charlie <charlieycw@gmail.com>
2014-11-20 16:23:23 -0800

    Item and ItemTest. Danny pull. Allday

--- src/com/engine/Item.java
+++ src/com/engine/Item.java
@@ -12,32 +12,40 @@ public class Item {
 	private String category;
 	private String company;
 	private int year;
-	private double price;
+	private Double price;
 	private int stock;
+	private ResultSet result;
+	private PreparedStatement ps;
 	
-	private Item() {
+	public Boolean exist;
+	
+	public Item() {
 		
 	}
 	
 	/* users chooses item and returns the stock price */
 	
-	private int checkStock(int upc) {
+	public int checkStock(int upc) {
 		
 		int stock = 0;
 		return stock;
 	}
 	
-	private void itemExist(int upc) {
+	public Boolean itemExist(int upc) {
 		
 		try {
 		String query = "SELECT * FROM cpsc304.Item WHERE upc=" + upc;
-		ResultSet result;
-		PreparedStatement ps = Engine.getInstance().getConnection().prepareStatement(query);
+		ps = Engine.getInstance().getConnection().prepareStatement(query);
 		result = ps.executeQuery();
+		
 		while (result.next() == false) {
-			addItem(upc, price);
+			// no item in result
+			exist = false;
 		}
-		updateItem();
+		
+		while (result.next() == true) {
+			exist = true;
+		}
 		
 		} 
 		catch (SQLException e) 
@@ -45,16 +53,49 @@ public class Item {
 			e.printStackTrace();
 			System.out.println("Failed to execute select from: " + upc + "\nError Message: " + e.getMessage());
 		}
-	}
-	
-	private int addItem(int upc, double price) {
-
-		return 0;
-	}
-	
-	private int updateItem() {
-		return 0;
-	}
 		
+		return exist;
+	}
 	
+	public void addOrUpdate() {
+		if (exist = false) {
+			addItem(upc, title, type, category, company, year, price, stock);
+		}
+		else
+			updateItem(upc, stock, price);
+	}
+	
+	public void addItem(int upc, String title, String type, String category, String company, int year, double price, int stock) {
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append( "(" ).append( upc ).append( ",'" ).append( title ).append( "','" );
+		stringBuilder.append( type ).append( "','" ).append( category ).append( "'," ).append( company );
+		stringBuilder.append( year ).append( "','" ).append( price ).append( "'," ).append( stock );
+		String values = stringBuilder.append( ")" ).toString();
+		
+		System.out.println(values);
+		
+		Engine.getInstance().getQueries().insertQuery(" Item ", values );
+	}
+	
+	public void updateItem(int upc, int stock, Double price) {
+		
+		//check if new price is given
+		if (price == 0) {
+			this.stock = stock;
+		}
+		else {
+			this.price = price;
+			this.stock = stock;	
+		}
+		
+		try {
+			String query = "UPDATE cpsc304.Item" + "SET price=" + price + "WHERE upc =" + upc;
+			ps = Engine.getInstance().getConnection().prepareStatement(query);
+			result = ps.executeQuery();
+		} catch (SQLException e) {
+			System.out.println("Failed to execute select from: " + upc + "\nError Message: " + e.getMessage());
+			e.printStackTrace();
+		}
+		}
 }
